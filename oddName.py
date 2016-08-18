935ac13f2b04883906d02e3c5b533be87bc9926a
karlee.gibson <karlee.gibson@my.jcu.edu.au>
2016-08-18 11:32:56 +1000

    Add name printing program

--- oddName.py
+++ oddName.py
@@ -1,1 +1,8 @@
 """Karlee Gibson"""
+
+name = input("Name: ")
+while name == "":
+    print("Sorry but you can't leave name blank")
+    name = input("Name: ")
+length = len(name)
+print(name[0:length:2])
